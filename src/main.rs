755c91b820d4f7d6a7026bb2ef1a4360f51a6eaf
aecsocket <aecsocket@tutanota.com>
2024-07-01 16:02:44 +0100

    this is a mess

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,28 @@
 #![doc = include_str!("../README.md")]
 
-mod util;
+// mod util;
 
 use std::{
     collections::hash_map::Entry,
-    ffi::OsStr,
-    fs, io,
+    fs,
     os::unix::fs::{FileTypeExt, OpenOptionsExt},
     path::{Path, PathBuf},
+    time::Duration,
 };
 
 use ahash::AHashMap;
 use anyhow::{anyhow, Context, Result};
 use clap::Parser;
-use evdev_rs::{
-    enums::{
-        BusType, EventCode, InputProp,
-        EV_ABS::*,
-        EV_KEY::{self, *},
-        EV_REL::*,
-        EV_SYN::*,
-    },
-    Device, DeviceWrapper, GrabMode, ReadFlag, UInputDevice, UninitDevice,
-};
 
+use evdev::{
+    uinput::{VirtualDevice, VirtualDeviceBuilder},
+    AbsInfo, AbsoluteAxisType, AttributeSet, Device, EventStream, EventType, InputEvent,
+    InputEventKind, Key, PropType, UinputAbsSetup,
+};
+use futures::{stream::FuturesUnordered, StreamExt};
 use log::{debug, error, info, warn};
 use notify::Watcher;
 use tokio::sync::mpsc;
-use util::{emit, enable_abs, enable_key, sync, Abs};
 
 /// Simulate a trackpad with your physical mouse
 ///
@@ -56,16 +51,16 @@ pub struct Args {
     pub input_deny: Vec<PathBuf>,
     /// Key code which activates 2-finger swiping mode
     #[arg(short = '2')]
-    pub swipe_2: Option<u32>,
+    pub swipe_2: Option<u16>,
     /// Key code which activates 3-finger swiping mode
     #[arg(short = '3', default_value = "277")]
-    pub swipe_3: Option<u32>,
+    pub swipe_3: Option<u16>,
     /// Key code which activates 4-finger swiping mode
     #[arg(short = '4')]
-    pub swipe_4: Option<u32>,
+    pub swipe_4: Option<u16>,
     /// Key code which activates 5-finger swiping mode
     #[arg(short = '5')]
-    pub swipe_5: Option<u32>,
+    pub swipe_5: Option<u16>,
     /// Resolution of the virtual trackpad
     ///
     /// A larger resolution means you have to move your mouse further to have
@@ -93,6 +88,7 @@ pub struct Args {
 type Never = std::convert::Infallible;
 
 const DEV_INPUT: &str = "/dev/input";
+const VIRTUAL_DEVICE_NAME: &str = "fukomaster virtual trackpad";
 
 #[derive(Debug, Clone)]
 enum DeviceEvent {
@@ -119,23 +115,16 @@ async fn main() -> Result<Never> {
         no_grab,
     } = Args::parse();
 
-    let to_trigger_key = |code: Option<u32>, fingers| -> Result<Option<EV_KEY>> {
-        code.map(|code| {
-            evdev_rs::enums::int_to_ev_key(code)
-                .ok_or(anyhow!("invalid {fingers}-finger swipe trigger key"))
-        })
-        .transpose()
-    };
-    let swipe_2 = to_trigger_key(swipe_2, 2)?;
-    let swipe_3 = to_trigger_key(swipe_3, 3)?;
-    let swipe_4 = to_trigger_key(swipe_4, 4)?;
-    let swipe_5 = to_trigger_key(swipe_5, 5)?;
+    let swipe_2 = swipe_2.map(Key::new);
+    let swipe_3 = swipe_3.map(Key::new);
+    let swipe_4 = swipe_4.map(Key::new);
+    let swipe_5 = swipe_5.map(Key::new);
 
     let grab = !no_grab;
 
     // setup
 
-    let (send_device_event, mut recv_device_event) = mpsc::unbounded_channel::<DeviceEvent>();
+    let (send_device_events, mut recv_device_events) = mpsc::unbounded_channel::<DeviceEvent>();
 
     // first enumerate what devices we already have
     // note that paths in DeviceEvents may not actually point to a device;
@@ -145,7 +134,7 @@ async fn main() -> Result<Never> {
         .with_context(|| format!("failed to list files under {DEV_INPUT:?}"))?
     {
         let entry = result.with_context(|| format!("failed to read file under {DEV_INPUT:?}"))?;
-        send_device_event
+        send_device_events
             .send(DeviceEvent::Added(entry.path()))
             .expect("channel should be open");
     }
@@ -159,7 +148,7 @@ async fn main() -> Result<Never> {
         }) => {
             for path in paths.into_iter() {
                 debug!("{path:?} created");
-                let _ = send_device_event.send(DeviceEvent::Added(path));
+                let _ = send_device_events.send(DeviceEvent::Added(path));
             }
         }
         Ok(notify::Event {
@@ -169,7 +158,7 @@ async fn main() -> Result<Never> {
         }) => {
             for path in paths.into_iter() {
                 debug!("{path:?} removed");
-                let _ = send_device_event.send(DeviceEvent::Removed(path));
+                let _ = send_device_events.send(DeviceEvent::Removed(path));
             }
         }
         Ok(_) => {}
@@ -180,24 +169,33 @@ async fn main() -> Result<Never> {
             );
         }
     })
-    .with_context(|| "failed to create watcher")?;
+    .with_context(|| format!("failed to create {DEV_INPUT:?} watcher"))?;
 
     dev_watcher
         .watch(Path::new(DEV_INPUT), notify::RecursiveMode::NonRecursive)
         .with_context(|| format!("failed to start watching {DEV_INPUT:?}"))?;
     info!("Watching {DEV_INPUT:?} for device changes");
 
-    let sink = create_trackpad(resolution).with_context(|| "failed to create virtual trackpad")?;
+    let mut sink = create_trackpad(resolution).with_context(|| "failed to create sink")?;
     // we need a slight delay after creating the input device
     // so that other processes (i.e. compositor) can recognize it
-    std::thread::sleep(std::time::Duration::from_millis(200));
+    tokio::time::sleep(Duration::from_millis(200)).await;
     info!("Created virtual trackpad");
-    info!("  devnode = {:?}", sink.devnode().unwrap());
-    info!("  syspath = {:?}", sink.syspath().unwrap());
+    let sys_path = sink
+        .get_syspath()
+        .with_context(|| "failed to get sys path of sink")?;
+    info!("  sys path = {sys_path:?}");
+    let mut dev_nodes = sink
+        .enumerate_dev_nodes()
+        .await
+        .with_context(|| "failed to enumerate dev nodes of sink")?;
+    while let Ok(Some(dev_node)) = dev_nodes.next_entry().await {
+        info!("  dev node = {dev_node:?}");
+    }
 
     simulate_trackpad(
-        &mut recv_device_event,
-        &sink,
+        &mut recv_device_events,
+        &mut sink,
         &input_allow,
         &input_deny,
         swipe_2,
@@ -218,7 +216,7 @@ fn init_logging() {
     builder.init();
 }
 
-fn create_trackpad(resolution: u16) -> Result<UInputDevice> {
+fn create_trackpad(resolution: u16) -> Result<VirtualDevice> {
     /*
     # Supported events:
     #   Event type 0 (EV_SYN)
@@ -304,84 +302,169 @@ fn create_trackpad(resolution: u16) -> Result<UInputDevice> {
     #   Property  type 2 (INPUT_PROP_BUTTONPAD)
     */
 
-    let dev = UninitDevice::new().ok_or(anyhow!("failed to create virtual device"))?;
-    dev.set_name("fukomaster virtual trackpad");
-    dev.set_bustype(BusType::BUS_VIRTUAL as u16);
+    // https://www.kernel.org/doc/html/v4.12/input/event-codes.html
+    // https://www.kernel.org/doc/html/v4.12/input/multi-touch-protocol.html
 
-    // randomly generated IDs
-    dev.set_vendor_id(0x4394);
-    dev.set_product_id(0xd1fc);
+    fn abs(min: i32, max: i32, resolution: i32) -> AbsInfo {
+        AbsInfo::new(0, min, max, 0, 0, resolution)
+    }
 
-    (|| {
-        // https://www.kernel.org/doc/html/v4.12/input/event-codes.html
-        // https://www.kernel.org/doc/html/v4.12/input/multi-touch-protocol.html
+    fn abs_with_max(max: i32) -> AbsInfo {
+        abs(0, max, 0)
+    }
 
-        dev.enable(InputProp::INPUT_PROP_POINTER)?;
-        dev.enable(EventCode::EV_SYN(SYN_REPORT))?;
-
-        enable_key(&dev, BTN_TOOL_FINGER)?;
-        enable_key(&dev, BTN_TOUCH)?;
-        enable_key(&dev, BTN_TOOL_DOUBLETAP)?;
-        enable_key(&dev, BTN_TOOL_TRIPLETAP)?;
-        enable_key(&dev, BTN_TOOL_QUADTAP)?;
-        enable_key(&dev, BTN_TOOL_QUINTTAP)?;
-
-        enable_abs(&dev, ABS_MT_SLOT, Abs::with_max(4))?; // max 5 touches
-        enable_abs(&dev, ABS_MT_TRACKING_ID, Abs::with_max(i32::MAX))?;
-
-        let resolution = i32::from(resolution);
-        enable_abs(
-            &dev,
-            ABS_MT_POSITION_X,
-            Abs::new(i32::MIN, i32::MAX, resolution),
-        )?;
-        enable_abs(
-            &dev,
-            ABS_MT_POSITION_Y,
-            Abs::new(i32::MIN, i32::MAX, resolution),
-        )?;
-
-        Ok::<(), io::Error>(())
-    })()
-    .with_context(|| "failed to initialize device")?;
-
-    let dev = UInputDevice::create_from_device(&dev)
-        .with_context(|| "failed to create initialized device")?;
+    let resolution = i32::from(resolution);
+    let dev = VirtualDeviceBuilder::new()?
+        .name(VIRTUAL_DEVICE_NAME)
+        .with_properties(&AttributeSet::from_iter([PropType::POINTER]))?
+        .with_keys(&AttributeSet::from_iter([
+            Key::BTN_TOOL_FINGER,
+            Key::BTN_TOUCH,
+            Key::BTN_TOOL_DOUBLETAP,
+            Key::BTN_TOOL_TRIPLETAP,
+            Key::BTN_TOOL_QUADTAP,
+            Key::BTN_TOOL_QUINTTAP,
+        ]))?
+        .with_absolute_axis(&UinputAbsSetup::new(
+            AbsoluteAxisType::ABS_MT_SLOT,
+            abs_with_max(4),
+        ))? // max 5 touches
+        .with_absolute_axis(&UinputAbsSetup::new(
+            AbsoluteAxisType::ABS_MT_TRACKING_ID,
+            abs_with_max(i32::MAX),
+        ))?
+        .with_absolute_axis(&UinputAbsSetup::new(
+            AbsoluteAxisType::ABS_MT_POSITION_X,
+            abs(i32::MIN, i32::MAX, resolution),
+        ))?
+        .with_absolute_axis(&UinputAbsSetup::new(
+            AbsoluteAxisType::ABS_MT_POSITION_Y,
+            abs(i32::MIN, i32::MAX, resolution),
+        ))?
+        .build()?;
     Ok(dev)
 }
 
 async fn simulate_trackpad(
-    recv_device_event: &mut mpsc::UnboundedReceiver<DeviceEvent>,
-    sink: &UInputDevice,
+    device_events: &mut mpsc::UnboundedReceiver<DeviceEvent>,
+    sink: &mut VirtualDevice,
     input_allow: &[PathBuf],
     input_deny: &[PathBuf],
-    swipe_2: Option<EV_KEY>,
-    swipe_3: Option<EV_KEY>,
-    swipe_4: Option<EV_KEY>,
-    swipe_5: Option<EV_KEY>,
+    swipe_2: Option<Key>,
+    swipe_3: Option<Key>,
+    swipe_4: Option<Key>,
+    swipe_5: Option<Key>,
     x_mult: f32,
     y_mult: f32,
     grab: bool,
 ) -> Result<Never> {
     enum State {
         Normal,
-        Swiping { x: i32, y: i32 },
+        Swiping { fingers: u8, x: i32, y: i32 },
     }
 
     impl State {
-        fn swiping() -> Self {
-            Self::Swiping { x: 0, y: 0 }
+        fn swiping(fingers: u8) -> Self {
+            Self::Swiping {
+                fingers,
+                x: 0,
+                y: 0,
+            }
         }
     }
 
-    let sink_devnode = sink
-        .devnode()
-        .with_context(|| "sink does not have a devnode")?;
-
     let mut state = State::Normal;
-    let mut devices = AHashMap::<PathBuf, Device>::new();
+    let mut devices = AHashMap::<PathBuf, EventStream>::new();
 
-    let futures = devices.iter().map(|(_, dev)| dev.file())
+    loop {
+        let mut input_events = devices
+            .iter_mut()
+            .map(|(path, events)| async move {
+                let res = events.next_event().await;
+                (path, events.device_mut(), res)
+            })
+            .collect::<FuturesUnordered<_>>();
+
+        tokio::select! {
+            Some(event) = device_events.recv() => {
+                drop(input_events);
+                match event {
+                    DeviceEvent::Added(path) => {
+                        info!("Added {path:?}");
+                    }
+                    DeviceEvent::Removed(path) => {
+                        if let Some(events) = devices.remove(&path) {
+                            if let Some(name) = events.device().name() {
+                                info!("Removed {name:?} ({path:?})");
+                            } else {
+                                info!("Removed {path:?}");
+                            }
+                        }
+                    }
+                }
+            }
+            Some((path, device, input)) = input_events.next() => {
+                let input = match input {
+                    Ok(input) => input,
+                    Err(err) => {
+                        warn!(
+                            "Failed to read events from {path:?}: {:#}",
+                            anyhow::Error::new(err)
+                        );
+                        continue;
+                    }
+                };
+
+                match state {
+                    State::Normal => {
+                        let mut try_start_swipe = |trigger: Option<Key>, fingers, btn_tool| -> Result<()> {
+                            if trigger
+                                .map(|key| input.kind() == InputEventKind::Key(key))
+                                .unwrap_or(false)
+                                && input.value() == 1
+                            {
+                                if grab {
+                                    if let Err(err) = device.grab() {
+                                        warn!(
+                                            "Failed to grab {path:?}, will not start swiping: {:#}",
+                                            anyhow::Error::new(err)
+                                        );
+                                        return Ok(());
+                                    }
+                                }
+
+                                start_swipe(sink, fingers, btn_tool)?;
+                                info!("Start swipe with {fingers} fingers on {path:?}");
+                                state = State::Swiping {
+                                    fingers,
+                                    x: 0,
+                                    y: 0,
+                                };
+                            }
+                            Ok(())
+                        };
+
+                        try_start_swipe(swipe_2, 2, Key::BTN_TOOL_DOUBLETAP)?;
+                        try_start_swipe(swipe_3, 3, Key::BTN_TOOL_TRIPLETAP)?;
+                        try_start_swipe(swipe_4, 4, Key::BTN_TOOL_QUADTAP)?;
+                        try_start_swipe(swipe_5, 5, Key::BTN_TOOL_QUINTTAP)?;
+                    }
+                    State::Swiping { fingers, x, y } => {}
+                }
+            }
+        }
+
+        // match (state, input) {
+        //     (State::Normal, event) if matches!(event.kind(), InputEventKind::Key(key)) => {}
+        //     (State::Normal, event)
+        //         if swipe_3.map(|key| event.kind() == InputEventKind::Key(key))(
+        //             State::Normal | State::Swiping { .. },
+        //             _,
+        //         ) => {}
+        // }
+    }
+
+    /*
 
     loop {
         tokio::select! {
@@ -406,9 +489,10 @@ async fn simulate_trackpad(
                 }
             }
         }
-    }
+    }*/
 }
 
+/*
 fn add_device(
     path: PathBuf,
     devices: &mut AHashMap<PathBuf, Device>,
@@ -473,7 +557,7 @@ fn add_device(
         info!("Added {path:?}");
     }
     Ok(())
-}
+}*/
 
 /*
 #[derive(Debug, Clone, Copy)]
@@ -546,20 +630,13 @@ fn simulate_trackpad(source: &mut Device, sink: &UInputDevice, config: Config) -
             (State::Normal | State::Swiping { .. }, _, _) => {}
         }
     }
+}*/
+
+fn abs_event(axis_type: AbsoluteAxisType, value: i32) -> InputEvent {
+    InputEvent::new_now(EventType::ABSOLUTE, axis_type.0, value)
 }
 
-fn start_swipe(
-    source: &mut Device,
-    sink: &UInputDevice,
-    fingers: i32,
-    btn_tool: EV_KEY,
-    grab: bool,
-) -> Result<()> {
-    if grab && source.grab(GrabMode::Grab).is_err() {
-        warn!("Failed to grab source device, will not start swiping for now");
-        return Ok(());
-    }
-
+fn start_swipe(sink: &mut VirtualDevice, fingers: u8, btn_tool: Key) -> Result<()> {
     /*
     E: 0.000001 0003 0039 8661	# EV_ABS / ABS_MT_TRACKING_ID   8661
     E: 0.000001 0003 0035 0690	# EV_ABS / ABS_MT_POSITION_X    690
@@ -580,24 +657,27 @@ fn start_swipe(
     E: 0.000001 0000 0000 0000	# ------------ SYN_REPORT (0) ---------- +0ms
     */
 
-    for finger in 0..fingers {
-        emit(sink, ABS_MT_SLOT, finger)?;
-        emit(sink, ABS_MT_TRACKING_ID, finger)?;
-        // (0, 0) is the center of the virtual trackpad
-        emit(sink, ABS_MT_POSITION_X, 0)?;
-        emit(sink, ABS_MT_POSITION_Y, 0)?;
-    }
-    emit(sink, BTN_TOUCH, 1)?;
-    emit(sink, btn_tool, 1)?;
-    sync(sink)?;
+    let events = (0..i32::from(fingers))
+        .flat_map(|finger| {
+            [
+                abs_event(AbsoluteAxisType::ABS_MT_SLOT, finger),
+                abs_event(AbsoluteAxisType::ABS_MT_TRACKING_ID, finger),
+                abs_event(AbsoluteAxisType::ABS_MT_POSITION_X, 0),
+                abs_event(AbsoluteAxisType::ABS_MT_POSITION_Y, 0),
+            ]
+        })
+        .chain([
+            InputEvent::new(EventType::KEY, Key::BTN_TOUCH.0, 1),
+            InputEvent::new(EventType::KEY, btn_tool.0, 1),
+        ]);
 
-    debug!("Started swiping");
+    sink.emit(&events.collect::<Vec<_>>())?;
     Ok(())
 }
 
 fn update_position(
-    dev: &UInputDevice,
-    fingers: i32,
+    sink: &mut VirtualDevice,
+    fingers: u8,
     x: i32,
     x_mult: f32,
     y: i32,
@@ -623,23 +703,19 @@ fn update_position(
     #[allow(clippy::cast_possible_truncation)]
     let y = ((y as f32) * y_mult) as i32;
 
-    for finger in 0..fingers {
-        emit(dev, ABS_MT_SLOT, finger)?;
-        emit(dev, ABS_MT_POSITION_X, x)?;
-        emit(dev, ABS_MT_POSITION_Y, y)?;
-    }
-    sync(dev)?;
+    let events = (0..i32::from(fingers)).flat_map(|finger| {
+        [
+            abs_event(AbsoluteAxisType::ABS_MT_SLOT, finger),
+            abs_event(AbsoluteAxisType::ABS_MT_POSITION_X, x),
+            abs_event(AbsoluteAxisType::ABS_MT_POSITION_Y, y),
+        ]
+    });
 
+    sink.emit(&events.collect::<Vec<_>>())?;
     Ok(())
 }
 
-fn stop_swipe(
-    source: &mut Device,
-    sink: &UInputDevice,
-    fingers: i32,
-    btn_tool: EV_KEY,
-    grab: bool,
-) -> Result<()> {
+fn stop_swipe(sink: &mut VirtualDevice, fingers: u8, btn_tool: Key) -> Result<()> {
     /*
     E: 2.992985 0000 0000 0000	# ------------ SYN_REPORT (0) ---------- +7ms
     E: 3.000143 0003 002f 0001	# EV_ABS / ABS_MT_SLOT          1
@@ -658,19 +734,18 @@ fn stop_swipe(
     E: 3.007174 0000 0000 0000	# ------------ SYN_REPORT (0) ---------- +7ms
     */
 
-    for finger in 0..fingers {
-        emit(sink, ABS_MT_SLOT, finger)?;
-        emit(sink, ABS_MT_TRACKING_ID, -1)?;
-    }
-    emit(sink, BTN_TOOL_FINGER, 0)?;
-    emit(sink, btn_tool, 0)?;
-    sync(sink)?;
+    let events = (0..i32::from(fingers))
+        .flat_map(|finger| {
+            [
+                abs_event(AbsoluteAxisType::ABS_MT_SLOT, finger),
+                abs_event(AbsoluteAxisType::ABS_MT_TRACKING_ID, -1),
+            ]
+        })
+        .chain([
+            InputEvent::new_now(EventType::KEY, Key::BTN_TOOL_FINGER.0, 0),
+            InputEvent::new_now(EventType::KEY, btn_tool.0, 0),
+        ]);
 
-    if grab && source.grab(GrabMode::Ungrab).is_err() {
-        warn!("Failed to ungrab source device, will still stop swiping");
-    }
-
-    debug!("Stopped swiping");
+    sink.emit(&events.collect::<Vec<_>>())?;
     Ok(())
 }
-*/
